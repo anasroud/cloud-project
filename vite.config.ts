2274bf489ec7ebb82d89e99c83ea458e6af907c5
Anas Roud <roud2@hotmail.com>
2025-04-19 15:01:19 -0500

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: 'cloud.anasroud.com',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
